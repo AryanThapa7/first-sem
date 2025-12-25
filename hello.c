491887bcf1db2fae4bcb52b73eef97fa7f767e99
Aryanthapa7 <aryanarthapa@gmail.com>
2025-12-25 10:47:50 +0545

    First armstrong meet

--- hello.c
+++ hello.c
@@ -2,7 +2,36 @@
 
 int main(){
 
-    printf("hello world");
+int num ;
+int digit ;
+int sum = 0 ;
+int temp ;
 
-    return 0;
+printf("Enter any number");
+scanf("%d", &num);
+
+temp = num ;
+
+while( num !=0){
+digit = num % 10 ; // last to digit 
+sum = sum + (digit*digit*digit);
+num = num / 10 ;    // decimal ma last ko number aucha ani hatcha 
 }
+
+if( sum == temp ){
+    printf("%d is an armstrong number", temp);
+}
+
+    else{
+        printf("&%d is not an armstrong number", temp);
+    
+    }
+
+    return 0 ;
+
+}
+
+
+
+
+
