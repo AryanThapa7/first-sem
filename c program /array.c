4c8a1eb12681b6c6c698c731f4e9e8a3e779bd8f
Aryanthapa7 <aryanarthapa@gmail.com>
2026-01-13 10:51:09 +0545

    intro to array

--- c program /array.c
+++ c program /array.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    int n;
-    printf("Enter size: ");
-    scanf("%d", &n); 
-    int array[n];
-    printf("Enter %d numbers\n", n);
-    for(int i = 0 ; i<n; i++){
-        scanf("%d", array[i]);
-    }
-    for(int i = 0; i<n; i++){
-        printf("%d", array[i]);
-    }
+int main(){
+    int  n ;
+    int even= 0 , odd= 0;
+
+    printf("Enter the number of elements");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements:\n");
+
+    for(int i=0; i<n ; i++){
+         scanf("%d", &arr[i]);
     
+         if(arr[i]%2==0)
+         even++;
+
+         else 
+         odd++;
+}
+
+    printf("Even numbers count = %d\n", even);
+    printf("Odd numbers count = %d\n", odd); 
+
     return 0;
 }
