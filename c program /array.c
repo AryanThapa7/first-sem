6d34f97584250824e11159b00983ff4d72e72c9c
Aryanthapa7 <aryanarthapa@gmail.com>
2026-01-21 10:55:33 +0545

    copying a string

--- c program /array.c
+++ c program /array.c
@@ -2,12 +2,21 @@
 #include <string.h>
 
 int main(){
-        char str[]="aryan";
-        int count = 0 ;
-        for(int i = 0 ; str[i]!='\0' ; i++){
-            count++ ; 
-        }
-        printf("The number of letters : %d\n" ,count);
-        return 0 ;
+    char str1[] = "aryan";
+    char str2[20];
+    int count = 0;
+    int i;
 
+    for(i = 0; str1[i] != '\0'; i++){
+        count++;
+    }
+    for(i = 0; str1[i] != '\0'; i++){
+        str2[i] = str1[i];
+    }
+
+    str2[i] = '\0';   
+    printf("The number of letters: %d\n", count);
+    printf("Copied string is: %s\n", str2);
+
+    return 0;
 }
