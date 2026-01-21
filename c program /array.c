03131dfeb982942185b27112563f2905e71f5bbe
Aryanthapa7 <aryanarthapa@gmail.com>
2026-01-21 10:36:55 +0545

    string

--- c program /array.c
+++ c program /array.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    int a[2][2], b[2][2], sum[2][2];
-    int i, j;
+int main(){
+        char str[20], ch = 'y';
+        int i = 0 ;
+        printf("Enter your name : ");
+        while(ch!='\n'){
+            ch=getchar();
+            str[i]=ch ;
+            i++;
+        }
+        str[i]='\0';
+        printf("Your name is : %s ", str);
+        return 0 ;
 
-    printf("Enter elements of first matrix:\n");
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
-            scanf("%d", &a[i][j]);
-        }
-    }
-    printf("Enter elements of second matrix:\n");
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
-            scanf("%d", &b[i][j]);
-        }
-    }
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
-            sum[i][j] = a[i][j] + b[i][j];
-        }
-    }
-    printf("Sum of matrices:\n");
-    for(i = 0; i < 2; i++) {
-        for(j = 0; j < 2; j++) {
-            printf("%d ", sum[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
 }
