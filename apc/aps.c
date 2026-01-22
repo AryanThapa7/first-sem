5d7089271977e96ae2f86e5bcb639ed27b281f4f
Aryanthapa7 <aryanarthapa@gmail.com>
2026-01-22 13:42:32 +0545

    Time complexity using function

--- apc/aps.c
+++ apc/aps.c
@@ -1,26 +1,26 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <time.h>
 
-int main(){
-        int n = 100000;
-        int i ;
-
-        clock_t start , end ;
+void calculateTime(int n) {
+        int i;
+        clock_t start, end;
         double cpu_time_used;
 
-        start = clock(); 
+        start = clock();
 
-        for(i=0; i < n ; i++){
-
-                int x = i*i ;
-        }  
+     for(i = 0; i < n; i++) {
+        int x = i * i;
+    }
 
         end = clock();
 
-        cpu_time_used = ((double)(end - start))/CLOCKS_PER_SEC;
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-        printf("Time taken for n=%d is %f seconds\n", n , cpu_time_used);
+        printf("Time taken for n=%d is %f seconds\n", n, cpu_time_used);
+}
 
-        return 0 ;
-
+    int main() {
+    int n = 100000;
+    calculateTime(n);    
+    return 0;
 }
