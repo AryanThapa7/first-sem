cd33efa2b919a10d93be453f0f97cf8676bba2bf
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-12 13:52:05 +0545

    pointers and array and functions

--- c program /class.c
+++ c program /class.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-// pointer and array in c 
-
-int main() {
-    int arr[5];
-    int *ptr = arr; // pointer to the first element of the array
-
-   
-    for (int i = 0; i < 5; i++) {
-        printf("enter element %d: ", i + 1);
-        scanf("%d", (ptr + i)); // accessing array elements using pointer arithmetic
+// function returning pointer 
+// syntax: data_type *function_name(parameters)
+// eg : float *getAverage(int a, int b){
+//     float *avg = (float *)malloc(sizeof(float));
+//     *avg = (a + b) / 2.0;
+//     return avg;
+// }
+// array of pointers
+// syntax: data_type *array_name[size]
+// eg : int *arr[5]; // array of 5 integer pointers
+int main(){
+    int *pa[3]; // array of 3 integer pointers
+    int i, a = 5 , b = 10 , c = 15;
+    pa[0] = &a; // assigning address of a to first pointer
+    pa[1] = &b; // assigning address of b to second pointer 
+    pa[2] = &c; // assigning address of c to third pointer
+    for(i=0 ; i < 3 ; i++){
+        printf("Value at pa[%d] = %d\n", i, *pa[i]); // dereferencing pointer to get value
     }
-    for(int i = 0; i < 5; i++) {
-        printf("element %d: %d\n", i + 1, *(ptr + i)); // accessing array elements using pointer arithmetic
-    }
-
     return 0;
 }
