a27019234635a7c94ef3f7e455f50c88e6e84491
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-12 13:59:15 +0545

    void pointer

--- c program /class.c
+++ c program /class.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
-// structure pointer
-struct student {
-    char name[20];
-    int age;
-    char grade;
-    float gpa;
-};  
-int main() {
-    struct student s1 = {"Alice", 20, 'A', 3.8};
-    struct student *ptr = &s1;
-
-    printf("Name: %s\n", ptr->name);
-    printf("Age: %d\n", ptr->age);
-    printf("Grade: %c\n", ptr->grade);
-    printf("GPA: %.2f\n", ptr->gpa);
-
-    return 0;
+// void pointer 
+int main(){
+int a = 10;
+float b = 3.14;
+char ch = 'A';
+void *ptr; // void pointer declaration
+ptr = &a; // pointing to an integer
+printf("Value of a: %d\n", *(int*)ptr); // dereferencing void pointer as integer
+ptr = &b; // pointing to a float
+printf("Value of b: %.2f\n", *(float*)ptr); // dereferencing void pointer as float
+ptr = &ch; // pointing to a char
+printf("Value of ch: %c\n", *(char*)ptr); // dereferencing void pointer as char
+return 0;
 }
