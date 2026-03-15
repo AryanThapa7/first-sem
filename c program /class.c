6bd1508ffe4bed711570c3969f63bced2c828436
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-15 17:46:38 +0545

    file handling in C

--- c program /class.c
+++ c program /class.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/* free () 
-this function is used to deallocate memory that was previously allocated by malloc, calloc, or realloc. It takes a pointer as an argument and frees the memory pointed to by that pointer. After calling free(), the pointer becomes invalid and should not be used again until it is assigned a new value.
+/* 
+file handling in C
+- file handling in C is done using the standard library functions provided in the <stdio.h> header file.
+- the main functions used for file handling in C are:
+1. fopen(): used to open a file and returns a pointer to the file.
+2. fclose(): used to close a file.
+3. fprintf(): used to write formatted data to a file.
+4. fscanf(): used to read formatted data from a file.
+5. fgetc(): used to read a single character from a file.
+6. fputc(): used to write a single character to a file.
+7. fgets(): used to read a string from a file.
+8. fputs(): used to write a string to a file.
+9. feof(): used to check if the end of a file has been reached.
+10. ferror(): used to check for errors in file handling.
 */
