8d0fd5f4cb43dd22ea96382f5484ddbaf089ed7d
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-15 11:14:57 +0545

    malloc

--- c program /class.c
+++ c program /class.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// void pointer 
-int main(){
-int a = 10;
-float b = 3.14;
-char ch = 'A';
-void *ptr; // void pointer declaration
-ptr = &a; // pointing to an integer
-printf("Value of a: %d\n", *(int*)ptr); // dereferencing void pointer as integer
-ptr = &b; // pointing to a float
-printf("Value of b: %.2f\n", *(float*)ptr); // dereferencing void pointer as float
-ptr = &ch; // pointing to a char
-printf("Value of ch: %c\n", *(char*)ptr); // dereferencing void pointer as char
-return 0;
+/*  Dynamic memory allocation 
+
+malloc() allocates a block of memory of the specified size and returns a pointer to the beginning of the block.
+ syntax : 
+    void* malloc(size_t size);
+        ptr = (castType*) malloc(sizeInBytes);
+        eg : 
+        int *ptr 
+        ptr = (int*) malloc(10 * sizeof(int)); 
+        this allocates memory for 10 integers and returns a pointer to the first integer in the block. The size of each integer is determined by sizeof(int).
+        returns null if the memory allocation fails
+
+ */
+int main(){ 
+    int *arr , size ;
+    printf("Enter the size of array : ");
+    scanf("%d",&size);
+
+    arr = (int*) malloc(size * sizeof(int)); // this allocates memory for 'size' integers and returns a pointer to the first integer in the block.
+    if ( arr == NULL){ // check if malloc failed to allocate memory
+        printf("Memory allocation failed\n");
+    }
+    for ( int i = 0; i < size; i++){
+        printf("Enter element %d : ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("The elements in the array are : ");
+    for ( int i = 0; i < size; i++){
+        printf("%d ",arr[i]);
+
+}
+    free(arr); // deallocate the memory allocated by malloc
+    return 0;
 }
